af69f9e18706171c0d089fef03070f5ecb363e44
ngeran <ngeran@gmail.com>
2026-01-09 13:02:29 -0500

    updated

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,8 +2,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'Vector Docs',
   tagline: 'Official Documentation',
@@ -15,30 +13,23 @@ const config: Config = {
     require.resolve('./src/client/sidebar-enhanced.js'),
   ],
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true, 
   },
 
-  // Set the production url of your site here
-  url: 'http://localhost:3000',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // FIXED: Your GitHub Pages production URL
+  url: 'https://ngeran.github.io',
+  // FIXED: The name of your GitHub repository with slashes
+  baseUrl: '/VectorDocs/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ngeran', // Usually your GitHub org/user name.
-  projectName: 'VectorDocs', // Usually your repo name.
-
+  organizationName: 'ngeran', 
+  projectName: 'VectorDocs', 
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -50,10 +41,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ngeran/VectorDocs/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -61,11 +50,6 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -78,15 +62,14 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Vector Docs',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Vector Docs Logo',
         src: 'img/logo.svg',
         className: 'navbar-custom-logo',
       },
@@ -99,7 +82,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/ngeran/VectorDocs',
           label: 'GitHub',
           position: 'right',
         },
@@ -143,12 +126,12 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/ngeran/VectorDocs',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Vector Docs, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
